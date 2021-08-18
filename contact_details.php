8b36daf1b4ef8548c382a2864e80e0437e0a105c
Alsim05 <alaisters3@gmail.com>
2021-08-18 16:45:17 +0100

    Committing and pushing!

---
+++ contact_details.php
@@ -0,0 +1,38 @@
+<?php
+
+// Declaring variables to store data to send to database later
+$host_name = 'localhost';
+$user_name = 'root';
+$pass_word = '';
+$db_name = 'website_data_db';
+
+// Creating a connection with the test_db database
+$conn = mysqli_connect($host_name, $user_name, $pass_word, $db_name);
+
+if(!$conn)
+{
+    die('Connection failed: '.mysqli_connect_error());
+}
+
+$fullName = $_POST["fullName"];
+$username = $_POST["username"];
+$password = $_POST["password"];
+$emailAddress = $_POST["emailAddress"];
+
+// Code providing data to be inserted intp database later
+$sql_data = "INSERT INTO contact_details VALUES('$fullName', '$username', '$password', '$emailAddress');";
+
+// Insert above data into database
+if($conn -> query($sql_data) === TRUE)
+{
+    echo 'Connection successfully made';
+}
+else
+{
+    echo "Error: ".$sql_data."<br>".$conn -> error;
+}
+
+echo 'Also testing out committing and pushing changes to Github';
+
+// Closing connection
+$conn -> close();
